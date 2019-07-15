f933d072c6ad95cf980572a4b93ee921854742f1
Axel Perossa <adperossa@gmail.com>
2019-07-15 20:56:00 -0300

    add stopwatch feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 /**
  * Displays the title and total players
@@ -9,6 +10,7 @@ const Header = (props) => {
     <header>
     <Stats players={props.players} />
     <h1> { props.title }</h1>
+    <Stopwatch />
     </header>
   );
 }
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class Stopwatch extends Component {
+
+  state = {
+    isRunning: false,
+    previousTime: 0,
+    elapsedTime: 0
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState( prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - prevState.previousTime)
+      }));
+    }
+  }
+
+  handleStopwatch = () => {
+
+    if (!this.state.isInitialized) { 
+      let intervalID = setInterval( () => this.tick(), 100);
+    }
+
+    this.setState( prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+
+    }
+  }
+
+  handleReset = () => {
+    this.setState({
+      elapsedTime: 0
+    });
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  render() {
+
+    const seconds = Math.floor( this.state.elapsedTime / 1000 );
+
+    return (
+      <div className="stopwatch">
+        <h2>Reloj</h2>
+        <span className="stopwatch-time">{ seconds }</span>
+        <button onClick={this.handleStopwatch}>
+          { this.state.isRunning ? 'Parar' : 'Iniciar' }
+        </button>
+        <button onClick={this.handleReset}>Resetear</button>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;
