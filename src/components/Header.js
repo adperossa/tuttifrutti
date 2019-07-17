5aa8aa59dd825d2888afacbf3f7b6bd9384cd494
Axel Perossa <adperossa@gmail.com>
2019-07-17 20:57:22 -0300

    add letter functionality TODO: better handle when no more letters left

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
 /**
  * Displays the title and total players
  */
-const Header = ({ title, players }) => {
-  return (
+class Header extends Component {
+  
+  state = {
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    letter: "A"
+  }
+
+  changeLetter = () => {
+    this.setState(prevState => {
+      const newLetters = prevState.letters.filter( l => l !== prevState.letter);
+      let newletter = newLetters[Math.floor(Math.random() * newLetters.length)];
+      if (newletter === undefined) newletter = "---";
+
+      return {
+        letters: newLetters,
+        letter: newletter
+      }
+  
+    });
+    
+  }
+  
+
+  render() {
+    const { title, players } = this.props;
+    return (
     <header>
-    <Stats players={players} />
-    <h1> { title }</h1>
-    <Stopwatch />
+      <Stats players={players} />
+      <div className="subheader">
+        <h1>{ title }</h1>
+        <h2>Letra Actual: <span className="letter">{this.state.letter}</span></h2>
+        <button onClick={this.changeLetter}>Nueva Letra</button>
+      </div>
+      <Stopwatch />
     </header>
-  );
+    );
+  }
 }
 
 export default Header;
