356f0bdc5a17e8cf364c9d2799b31183612b1468
Axel Perossa <adperossa@gmail.com>
2019-07-18 17:37:40 -0300

    publish project

---
+++ docs/precache-manifest.f169e275883e73805cfab522875ea3fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf89e613f6894fcd4c2eb0a5c5b926c0",
+    "url": "/tuttifrutti/index.html"
+  },
+  {
+    "revision": "9d19d1155c92152781d3",
+    "url": "/tuttifrutti/static/css/main.819fe886.chunk.css"
+  },
+  {
+    "revision": "66f395d302fbe0e167c4",
+    "url": "/tuttifrutti/static/js/2.b6fe4141.chunk.js"
+  },
+  {
+    "revision": "9d19d1155c92152781d3",
+    "url": "/tuttifrutti/static/js/main.902ccfb3.chunk.js"
+  },
+  {
+    "revision": "704de3b03c824af1164f",
+    "url": "/tuttifrutti/static/js/runtime~main.25154991.js"
+  }
+]);
